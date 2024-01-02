f2281bc8d81604fe87b9b2ed7e384025a36303c0
Juan Villegas <juanvj0425@gmail.com>
2024-01-02 15:50:47 -0500

    feat: tech stack with 3d spheres

--- src/constants/index.js
+++ src/constants/index.js
@@ -101,15 +101,15 @@ const technologies = [
     icon: threejs,
   },
   {
-    name: "git",
+    name: "Git",
     icon: git,
   },
   {
-    name: "figma",
+    name: "Figma",
     icon: figma,
   },
   {
-    name: "docker",
+    name: "Docker",
     icon: docker,
   },
 ];
