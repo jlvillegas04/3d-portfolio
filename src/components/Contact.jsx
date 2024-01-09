a6a3885cc305ce2b0663b747b942e51d2a9486af
Juan Villegas <juanvj0425@gmail.com>
2024-01-08 19:33:37 -0500

    feat: form email submission logic

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,9 +17,49 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_qydo3hp",
+        "template_scy08ls",
+        {
+          from_name: form.name,
+          to_name: "Juan",
+          from_email: form.email,
+          to_email: "juanvj0425@gmail.com",
+          message: form.message,
+        },
+        "vm8KaygQoZ9SG53Rm"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
